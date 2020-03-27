048dca6e993cd58b8da912126245c91bbdd99611
Jun Kato <i@junkato.jp>
2020-03-27 16:47:29 +0900

    Move pages into their own router

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import fs = require("fs");
 import process from "process";
 import Box from "./box";
 
+import { Pages } from "./routes/pages";
+
 let jsonPath = process.env.PAGES_JSON;
 const exists = fs.existsSync(jsonPath);
 if (!jsonPath || !exists) {
@@ -22,22 +24,5 @@ export const register = (app: express.Application) => {
   app.get("/", (req, res) => {
     res.render("index", { box });
   });
-
-  // XXX: sb2md appends links to `.md`...
-  app.get(/^\/(.+).md$/, (req, res) => {
-    const pageName: string = req.params[0];
-    res.redirect(`/${pageName}`);
-  });
-
-  app.get(/^\/(.+)$/, (req, res) => {
-    const pageName: string = req.params[0];
-    const page = box.getPage(pageName);
-    if (!page) {
-      res.status(404);
-      res.setHeader("Content-Type", "text/plain; charset=UTF-8");
-      res.send(`Page ${pageName} is not found`);
-      return;
-    }
-    res.render("page", { box, page });
-  });
+  app.use("/pages", new Pages(box).router);
 };
---
+++ src/routes/pages.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response, Router } from "express";
+import Box from "../box";
+
+export class Pages {
+  public router: Router;
+  constructor(private box: Box) {
+    this.router = Router();
+    this.initialize();
+  }
+  private initialize() {
+    // XXX: sb2md appends links to `.md`...
+    this.router.get(/^\/(.+).md$/, this.redirect.bind(this));
+    this.router.get(/^\/(.+)$/, this.fetch.bind(this));
+  }
+  private fetch(req: Request, res: Response, next: NextFunction) {
+    const { box } = this;
+    const pageName: string = req.params[0];
+    const page = box.getPage(pageName);
+    if (!page) {
+      res.status(404);
+      res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+      res.send(`Page ${pageName} is not found`);
+      return;
+    }
+    res.render("page", { box, page });
+  }
+  private redirect(req: Request, res: Response, next: NextFunction) {
+    const pageName: string = req.params[0];
+    res.redirect(`/pages/${pageName}`);
+  }
+}
