a0b61926e2e4c0b6df803ee1ab4f88f981d4475d
Jun Kato <i@junkato.jp>
2020-03-27 18:11:28 +0900

    Add .env support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import path from "path";
-import * as register from "./routes";
+import { register } from "./routes";
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-register.register(app);
+register(app);
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
--- src/options.ts
+++ src/options.ts
@@ -1,14 +1,40 @@
 import { IBoxOptions } from "./box";
 import { IAllOptions } from "./routes/all";
 
-const options: IBoxOptions & IAllOptions = {
+const opening = process.env.PAGE_OPENING
+  || "[* Section to show]";
+const endings = [
+  "#diary",
+  process.env.PAGE_ENDING || "[* Section to hide]"
+];
+
+const options: IBoxOptions & IAllOptions = process.env.PAGE_OPTIONS ? {
   // only show `2020-03-27`-like pages in /all
   filter: (pages) =>
     pages.filter((p) =>
       /^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/.test(p.title)),
 
-  // hide irrelevant sections?
-  linesProcessor: (lines) => lines
-};
+  // hide irrelevant sections
+  linesProcessor: (lines) => {
+    const res = [lines[0]];
+    let started = false;
+    let finished = false;
+    for (let i = 1; i < lines.length; i ++) {
+      const line = lines[i];
+      let active = false;
+      if (line === opening) {
+        started = true;
+      } else if (started && !finished) {
+        if (endings.includes(line)) {
+          finished = true;
+        } else {
+          active = true;
+        }
+      }
+      res.push(active ? line : `<!-- ${line} -->`);
+    }
+    return res;
+  }
+} : {};
 
 export default options;
